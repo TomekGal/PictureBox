b98949e68c6800ddcaa09c3d6023f4521ae02fa6
TomekGal <75546362+TomekGal@users.noreply.github.com>
2021-03-09 20:52:46 +0100

    Add project files

---
+++ PictureBox/PictureBox.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace PictureBox
+{
+    partial class PictureBox
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btChoosePicture = new System.Windows.Forms.Button();
+            this.btClearImage = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(24, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(465, 426);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btChoosePicture
+            // 
+            this.btChoosePicture.Location = new System.Drawing.Point(572, 160);
+            this.btChoosePicture.Name = "btChoosePicture";
+            this.btChoosePicture.Size = new System.Drawing.Size(135, 36);
+            this.btChoosePicture.TabIndex = 1;
+            this.btChoosePicture.Text = "Choose Picture";
+            this.btChoosePicture.UseVisualStyleBackColor = true;
+            this.btChoosePicture.Click += new System.EventHandler(this.btChoosePicture_Click);
+            // 
+            // btClearImage
+            // 
+            this.btClearImage.Location = new System.Drawing.Point(572, 247);
+            this.btClearImage.Name = "btClearImage";
+            this.btClearImage.Size = new System.Drawing.Size(135, 36);
+            this.btClearImage.TabIndex = 2;
+            this.btClearImage.Text = "Clear Image";
+            this.btClearImage.UseVisualStyleBackColor = true;
+            this.btClearImage.Click += new System.EventHandler(this.btClearImage_Click);
+            // 
+            // PictureBox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btClearImage);
+            this.Controls.Add(this.btChoosePicture);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "PictureBox";
+            this.Text = "PictureBox";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btChoosePicture;
+        private System.Windows.Forms.Button btClearImage;
+    }
+}
+
---
+++ PictureBox/PictureBox.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace PictureBox
+{
+    public partial class PictureBox : Form
+    {
+
+        public static string filekeepPath =
+           Path.Combine
+              (Environment.CurrentDirectory, "filekeepPath.txt");
+      
+
+        public PictureBox()
+        {
+
+            InitializeComponent();
+
+            if (File.Exists(filekeepPath))
+
+            {
+                var serializer = new XmlSerializer(typeof(string));
+                using (var streamReader = new StreamReader(filekeepPath))
+
+                {
+                    var filePath = (string)serializer.Deserialize(streamReader);
+                    ShowMyImage(filePath);
+                    streamReader.Close();
+            
+                }
+
+            }
+            else
+          
+                OpenFileEmpty();
+  
+        }
+
+
+        private void btChoosePicture_Click(object sender, EventArgs e)
+        {
+            OpenFile();
+        }
+      
+        public void ShowMyImage(string fileToDisplay)
+        {
+           
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.Visible = true;
+            pictureBox1.Image = Image.FromFile(fileToDisplay);
+            pictureBox1.BackgroundImage = pictureBox1.Image;
+            pictureBox1.Refresh();
+
+        }
+
+
+        public void OpenFile()
+        {
+
+           
+            OpenFileDialog openFile = new OpenFileDialog();
+
+            openFile.Filter = " jpg files (*.jpg)|*.jpg|bnp files (*.bnp)|*.bnp|cng files (*.cng)|*.cng";
+            openFile.RestoreDirectory = true;
+
+            if (openFile.ShowDialog() == DialogResult.OK)
+            {
+
+                var filePath = openFile.FileName;
+                ShowMyImage(filePath);
+
+                var serializer = new XmlSerializer(typeof(string));
+                using (var streamWriter = new StreamWriter(filekeepPath))
+
+                {
+                    serializer.Serialize(streamWriter, filePath);
+                    streamWriter.Close();
+                }
+            }
+            
+        }
+
+        public void OpenFileEmpty()
+        {
+          
+            OpenFileDialog openFile = new OpenFileDialog();
+
+            openFile.Filter = " jpg files (*.jpg)|*.jpg|bnp files (*.bnp)|*.bnp|cng files (*.cng)|*.cng";
+            openFile.RestoreDirectory = true;
+        
+            if (openFile.ShowDialog() == DialogResult.OK)
+            {
+                
+               var filePath = openFile.FileName;
+                ShowMyImage(filePath);
+                
+                var serializer = new XmlSerializer(typeof(string));
+                using (var streamWriter = new StreamWriter(filekeepPath))
+
+                {
+                    serializer.Serialize(streamWriter, filePath);
+                    streamWriter.Close();
+                   
+                }
+            }
+        }
+
+        private void btClearImage_Click(object sender, EventArgs e)
+        {
+            
+            pictureBox1.Hide();
+           
+        }
+    }
+}
+    
+
+
+
